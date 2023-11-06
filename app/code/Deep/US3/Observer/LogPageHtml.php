9077fb95acecb0ddc022625a0d9bb5300d458ccd
deephbwsl <deep.jadhv@hbwsl.com>
2023-11-06 15:21:59 +0530

    log html of page to system.log file using event

--- app/code/Deep/US3/Observer/LogPageHtml.php
+++ app/code/Deep/US3/Observer/LogPageHtml.php
@@ -1,25 +1,22 @@
 <?php
+
 namespace Deep\US3\Observer;
 
-use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Psr\Log\LoggerInterface;
 
-class LogPageHtml implements ObserverInterface {
+class LogPageHtml implements ObserverInterface
+{
     protected $logger;
 
-    public function __construct(LoggerInterface $logger) {
+    public function __construct(LoggerInterface $logger)
+    {
         $this->logger = $logger;
     }
 
-    public function execute(Observer $observer) {
-        $response = $observer->getEvent()->getData('response');
-        
-        if ($response instanceof \Magento\Framework\App\Response\Http) {
-            $html = $response->getBody();
-            $this->logger->info("HTML Content: " . $html);
-        } else {
-            $this->logger->info("Response is not of type \Magento\Framework\App\Response\Http.");
-        }
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $html = $observer->getEvent()->getData('response')->getBody();
+        $this->logger->info("Page HTML: " . $html);
     }
 }
